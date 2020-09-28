6afa5b1f32664e0362b3f91f0b01e32ffd34e0f8
unknown <ynarula678@gmail.com>
2020-09-28 21:39:57 +0530

    Back to top

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const signup = document.querySelector('.signup');
-const modal = document.querySelector('.form-modal');
-const close = document.querySelector('.close');
-signup.addEventListener('click',()=>{
-  modal.style.display='flex';
-})
-close.addEventListener('click',()=>{
-  modal.style.display='none';
-})
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
