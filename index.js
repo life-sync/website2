26eab39ebc5488c70fb71c600060d2a9e6ca5975
unknown <ynarula678@gmail.com>
2020-10-23 23:41:03 +0530

    Nav fixes

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ signup.addEventListener('click',()=>{
 close.addEventListener('click',()=>{
   modal.style.display='none';
 })
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+function openSlideMenu(){
+  document.getElementById('menu').style.width = '100%';
+  document.getElementById('content').style.marginLeft = '100%';
+}
+function closeSlideMenu(){
+  document.getElementById('menu').style.width = '0';
+  document.getElementById('content').style.marginLeft = '0';
+}
