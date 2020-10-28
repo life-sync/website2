26150378d19dc4ad39a92255f1cdf2792f4a4af9
unknown <ynarula678@gmail.com>
2020-10-28 16:55:31 +0530

    Typing fixes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//modal
 const signup = document.querySelector('.signup');
 const modal = document.querySelector('.form-modal');
 const close = document.querySelector('.close');
@@ -7,6 +8,7 @@ signup.addEventListener('click',()=>{
 close.addEventListener('click',()=>{
   modal.style.display='none';
 })
+//nav
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
@@ -19,3 +21,41 @@ function closeSlideMenu(){
   document.getElementById('menu').style.width = '0';
   document.getElementById('content').style.marginLeft = '0';
 }
+//typing
+var Writer = function(selector, text, infinite) {
+  this.config = {
+    selector: selector,
+    text: text,
+    infinite: infinite
+  };
+  this.interval = null;
+  this.start = function() {
+    var _this = this;
+    var cnt = _this.config.text;
+    var txt = "";
+    var i = 0; 
+    time = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
+    _this.interval = setInterval(function() {
+      if (txt.length == cnt.length) {
+        if(_this.config.infinite){
+          txt = "";
+          i = 0; 
+        }else{
+          _this.stop();
+        }
+      } else {
+        txt += cnt[i];
+        i++;
+      }
+      document.querySelector(_this.config.selector).innerHTML = txt;
+    }, time);
+  };
+  this.stop = function() {
+    var _this = this;
+    clearInterval(_this.interval);
+    document.querySelector(_this.config.selector).innerHTML = _this.config.text;
+  };
+};
+var text = "Learn~Observe~Create";
+var writer = new Writer("#text", text, true);
+writer.start();
