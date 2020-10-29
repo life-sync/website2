ff15037d235f9b63ea1018885507a73758bf17ba
unknown <ynarula678@gmail.com>
2020-10-29 23:30:50 +0530

    Back to top btn

--- index.js
+++ index.js
@@ -61,3 +61,20 @@ var writer = new Writer("#text", text, false);
 writer.start();
 //btn ripple
 //loader
+//back-to-top 
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
