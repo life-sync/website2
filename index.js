0b58b1f14054c8c1da5cc9463e225de51bc0a0df
Yuvraaj Narula <ynarula678@gmail.com>
2021-01-02 01:19:39 +0530

    scroll indicator

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+window.addEventListener('scroll', moveScrollIndicator);
+const scrollIndicatorElt = document.getElementById('scrollIndicator');
+const maxHeight = window.document.body.scrollHeight - window.innerHeight;
+function moveScrollIndicator(e){
+  const percentage = ((window.scrollY) / maxHeight) * 100;
+  scrollIndicatorElt.style.width = percentage + '%';
+}
