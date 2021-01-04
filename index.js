0b5126c6be2a91cfbf10efd249b32e5898defd9b
Yuvraaj Narula <ynarula678@gmail.com>
2021-01-05 00:24:05 +0530

    back to top btn

--- index.js
+++ index.js
@@ -44,3 +44,20 @@ var Writer = function(selector, text, infinite) {
 var text = "Learn~Observe~Create";
 var writer = new Writer("#text", text, false);
 writer.start();
+//back-to-top 
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
