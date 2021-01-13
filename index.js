8c1e8365ee2effa57831260cf479201f06c620d4
Yuvraaj Narula <ynarula678@gmail.com>
2021-01-14 00:12:11 +0530

    preloader

--- index.js
+++ index.js
@@ -84,3 +84,14 @@ $(window).scroll(function(){
   }
 });*/
 //loader
+var preload = document.createElement("div");
+preload.className = "preloader";
+preload.innerHTML =
+  '<p class="hello"></p><div id="preloader"><div id="loader"></div></div>';
+document.body.appendChild(preload);
+window.addEventListener("load", function() {
+  preload.className += " fade";
+  setTimeout(function() {
+    preload.style.display = "none";
+  }, 40000);
+});
